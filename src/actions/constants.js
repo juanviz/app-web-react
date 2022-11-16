af89227f46ad96e97c57159360a4277d6b7e3a51
Juan Vicente Herrera Ruiz de Alejo <juaherre@redhat.com>
2022-11-16 11:36:57 +0100

    first commit

---
+++ src/actions/constants.js
@@ -0,0 +1,16 @@
+const constants = {
+    /* identity */
+    SIGN_IN: 'SIGN_IN',
+    SIGN_IN_SILENT: 'SIGN_IN_SILENT',
+    SIGN_OUT: 'SIGN_OUT',
+    CHANGE_PASSWORD: 'CHANGE_PASSWORD',
+    RESET_PASSWORD: 'RESET_PASSWORD',
+
+    /* alerts */
+    DISMISS_MESSAGE: 'DISMISS_MESSAGE',
+
+    /* products */
+    GET_PRODUCTS: 'GET_PRODUCTS'
+};
+
+export default constants;
